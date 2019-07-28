124b6137b58cb295e33e1ff540b66831f41f2fd3
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-07-28 19:50:15 -0300

    Angular Exercício 1 Diego Henrique Silva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+ nomes: string[] = ['joao','maria','jose','pedro','felipe','carlos'];
+
 }
